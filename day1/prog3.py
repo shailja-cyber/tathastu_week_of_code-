8b07fc721e216d76f622dfa6f3451d6bffa7df3e
shailja <62753087+shailja-cyber@users.noreply.github.com>
2020-05-24 03:22:47 +0530

    Create prog3.py

---
+++ day1/prog3.py
@@ -0,0 +1,5 @@
+num1= int(input("enter a number"))
+num2= int(input("enter another number"))
+print("number1 is",num1,"and number2 is ",num2)
+num1,num2=num2,num1
+print("number1 is",num1,"and number2 is ",num2)
