5d3194e28f8057753fd64fc6daab81e68d378c32
shailja <62753087+shailja-cyber@users.noreply.github.com>
2020-05-26 19:58:35 +0530

    Create prog2.py

---
+++ day3/prog2.py
@@ -0,0 +1,11 @@
+from itertools import permutations 
+import string
+
+s = input("enter a string ")
+a = string.ascii_letters 
+p = permutations(s) 
+d = [] 
+for i in list(p): 
+    if (i not in d): 
+        d.append(i) 
+        print(''.join(i))
